8044875bc1bfa628d4ae4160d7fedec99168ee4a
centuryorder <yohan_selv@hotmail.com>
2016-12-03 12:35:44 -0500

    Added test file

---
+++ ButtonMOD/ButtonMOD.ino
@@ -0,0 +1,77 @@
+/*
+  Button
+ 
+ Turns on and off a light emitting diode(LED) connected to digital  
+ pin 13, when pressing a pushbutton attached to pin 2. 
+ 
+ 
+ The circuit:
+ * LED attached from pin 13 to ground 
+ * pushbutton attached to pin 2 from +3.3V
+ * 10K resistor attached to pin 2 from ground
+ 
+ * Note: on most Arduinos there is already an LED on the board
+ attached to pin 13.
+ 
+ 
+ created 2005
+ by DojoDave <http://www.0j0.org>
+ modified 30 Aug 2011
+ by Tom Igoe
+ modified Apr 27 2012
+ by Robert Wessels
+ 
+ This example code is in the public domain.
+ 
+ http://www.arduino.cc/en/Tutorial/Button
+ */
+// most launchpads have a red LED
+#define LED RED_LED
+
+//see pins_energia.h for more LED definitions
+//#define LED GREEN_LED
+
+// constants won't change. They're used here to 
+// set pin numbers:
+const int buttonPin = PUSH2;     // the number of the pushbutton pin
+const int ledPin =  GREEN_LED;      // the number of the LED pin
+boolean pushed = false;
+
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(P1_0, OUTPUT);
+  Serial.println(Pin
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);      
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+}
+
+void loop(){
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+  while( pushed != true)
+  {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    if(buttonState == HIGH)
+  {
+    if(pushed == false)
+    {
+      pushed = true;
+    }
+    else
+    {
+      pushed = false;
+    }
+  }
+  }
+}
